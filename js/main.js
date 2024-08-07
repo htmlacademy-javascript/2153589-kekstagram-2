26da3c6d522dac77372eb8675feca9a4d30b77a9
Yuriy Frolov <yfrolou@mail.ru>
2024-08-07 13:07:36 +0300

    Удаляет избыточные модули

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-import { getSettings } from './data.js';
-import { createPhoto } from './create-photo.js';
+import { generatePhotosList } from './data.js';
 
-const generatePhotosList = (length) => Array.from({ length }, createPhoto);
-
-generatePhotosList(getSettings().DESCRIPTION_MAX_COUNT);
+generatePhotosList();
