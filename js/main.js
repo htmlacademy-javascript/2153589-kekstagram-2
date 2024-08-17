13ecf1db0b8e93a187565d7fe97260ac3a27287f
Yuriy Frolov <yfrolou@mail.ru>
2024-08-17 13:59:33 +0300

    Реализует сценарий просмотра фото в полном размере. Вариант 2. Добавляет добавление комментариев

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 import { arrayToObject } from './util.js';
 import { generatePhotosList } from './data.js';
-import { renderPictures } from './pictures.js';
-import { renderBigPicture } from './picture-detail.js';
-import { renderCommentList } from './comments.js';
+import { renderPictures } from './gallery.js';
+import { renderPreview } from './picture-detail.js';
 
 const pictures = generatePhotosList();
 renderPictures(pictures);
 
 const picturesObject = arrayToObject(pictures);
-renderBigPicture(picturesObject, renderCommentList);
+renderPreview(picturesObject);
 
 
--- js/picture-detail.js
+++ js/picture-detail.js
@@ -1,10 +1,29 @@
-const comments = document.querySelector('.social__comments');
 const picture = document.querySelector('.big-picture');
 const close = document.querySelector('.big-picture__cancel');
 const gallery = document.querySelector('.pictures');
 
+const createCommenNode = (obj) => {
+  const node = document.createElement('li');
+  node.classList.add('social__comment');
+
+  const image = document.createElement('img');
+  image.width = 35;
+  image.height = 35;
+  image.classList.add('social__picture');
+  image.src = obj.avatar;
+  image.alt = obj.name;
+
+  const text = document.createElement('p');
+  text.classList.add('social__text');
+  text.textContent = obj.message;
+
+  node.append(image);
+  node.append(text);
+
+  return node;
+};
+
 const closeBigPicture = () => {
-  comments.innerHTML = '';
   picture.classList.add('hidden');
   document.body.classList.remove('modal-open');
 };
@@ -17,35 +36,73 @@ document.addEventListener('keydown', (evt) => {
   }
 });
 
-const renderBigPicture = (obj, cb) => {
-  gallery.addEventListener('click', (evt) => {
-    if (evt.target.closest('.picture')) {
-      evt.preventDefault();
-      const target = obj[evt.target.parentNode.dataset.id];
+const renderBigPicture = (pictureData) => {
+  picture.classList.remove('hidden');
+  const commentsContainer = picture.querySelector('.social__comments');
+  commentsContainer.innerHTML = '';
+  const commentsLoader = picture.querySelector('.comments-loader');
+  commentsLoader.classList.remove('hidden');
+  picture.querySelector('.big-picture__img img').src = pictureData.url;
+  picture.querySelector('.social__caption').textContent = pictureData.description;
+  picture.querySelector('.likes-count').textContent = pictureData.likes;
+  picture.querySelector('.social__comment-total-count').textContent = pictureData.comments.length;
+  let step = 0;
+  const addComments = () => {
+    const commentCountContainer = picture.querySelector('.social__comment-count');
+    const commentList = pictureData.comments;
 
-      picture.classList.remove('hidden');
-      picture.querySelector('.big-picture__img img').src = target.url;
-      picture.querySelector('.social__caption').textContent = target.description;
-      picture.querySelector('.likes-count').textContent = target.likes;
-      let shownComments = 0;
-
-      if (target.comments.length === 1) {
-        shownComments = 1;
-      } else if (target.comments.length > 1) {
-        shownComments = 2;
-      }
-
-      picture.querySelector('.social__comment-shown-count').textContent = shownComments;
-      picture.querySelector('.social__comment-total-count').textContent = target.comments.length;
-      //picture.querySelector('.social__comment-count').classList.add('hidden');
-      picture.querySelector('.comments-loader').classList.add('hidden');
-      document.body.classList.add('modal-open');
-      cb(target.comments, shownComments);
+    if (commentList.length === 0) {
+      commentsContainer.innerHTML = '<p class="comments-message-empty">Комментариев нет</p>';
+      commentCountContainer.classList.add('hidden');
+      commentsLoader.classList.add('hidden');
+      return;
     }
+    commentsContainer.innerHTML = '';
+    commentCountContainer.classList.remove('hidden');
+    step = (commentsContainer.children.length + 5) >= commentList.length ? commentList.length : step + 5;
+    const commentFragment = document.createDocumentFragment();
+
+    commentList.slice(0, step).forEach((item) => {
+      const comment = createCommenNode(item);
+      commentFragment.append(comment);
+    });
+
+    commentsContainer.append(commentFragment);
+    picture.querySelector('.social__comment-shown-count').textContent = commentsContainer.children.length;
+
+    if (commentsContainer.children.length >= commentList.length) {
+      commentsLoader.classList.add('hidden');
+    } else {
+      commentsLoader.classList.remove('hidden');
+    }
+  };
+
+  addComments();
+  commentsLoader.addEventListener('click', addComments);
+  document.body.classList.add('modal-open');
+};
+
+const renderPreview = (obj) => {
+  gallery.addEventListener('click', (evt) => {
+    const parent = evt.target.closest('.picture');
+    if (parent) {
+      evt.preventDefault();
+    } else {
+      return;
+    }
+
+    const targetId = parent.dataset.id;
+
+    if (!targetId) {
+      return;
+    }
+
+    const data = obj[targetId];
+    renderBigPicture(data);
   });
 };
 
 export {
-  renderBigPicture
+  renderPreview
 };
 
