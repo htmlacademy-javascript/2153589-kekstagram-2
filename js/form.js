27b367e1f89f1d0ff55584c49c518d2c09ea46ea
Yuriy Frolov <yfrolou@mail.ru>
2024-08-23 18:19:21 +0300

    Исправляет form.js

--- js/form.js
+++ js/form.js
@@ -3,43 +3,49 @@ import { isEscapeKeydown } from './util.js';
 const imageUploadInput = document.querySelector('.img-upload__input');
 const imageUploadOverlay = document.querySelector('.img-upload__overlay');
 const imageUploadCancel = document.querySelector('.img-upload__cancel');
-const previewEffects = document.querySelectorAll('.effects__preview');
-const uploadForm = document.querySelectorAll('#upload-select-image');
-
-const setPreviewEffects = () => {
-  previewEffects.forEach((item) => {
-    item.style.backgroundImage = `url(${imageUploadInput.value})`;
-  });
-};
+const uploadForm = document.querySelector('#upload-select-image');
+const hashtagInput = document.querySelector('.text__hashtags');
+const commentInput = document.querySelector('.text__description');
 
 const closeUploadForm = () => {
   imageUploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
-  uploadForm.reset();
-  imageUploadCancel.removeEventListener('click', closeUploadForm);
-  document.removeEventListener('keydown', onEscapeKeydownForm);
+  imageUploadInput.value = '';
+  imageUploadCancel.removeEventListener('click', onButtonResetClick);
+  document.removeEventListener('keydown', onDocumentEscapeKeydown);
 };
 
 const openUploadForm = () => {
   imageUploadOverlay.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  document.querySelector('.img-upload__preview img').src = imageUploadInput.value;
-  setPreviewEffects();
-  imageUploadCancel.addEventListener('click', closeUploadForm);
-  document.addEventListener('keydown', onEscapeKeydownForm);
+  imageUploadCancel.addEventListener('click', onButtonResetClick);
+  document.addEventListener('keydown', onDocumentEscapeKeydown);
 };
 
+function onButtonResetClick(evt) {
+  evt.preventDefault();
+  uploadForm.reset();
+  closeUploadForm();
+}
 
-function onEscapeKeydownForm(evt) {
-  if (isEscapeKeydown(evt) && document.activeElement.className !== 'text__hashtags') {
-    closeUploadForm();
+function onDocumentEscapeKeydown(evt) {
+  if (isEscapeKeydown(evt)) {
+    evt.preventDefault();
+
+    if (document.activeElement === hashtagInput || document.activeElement === commentInput) {
+      evt.stopPropagation();
+    } else {
+      uploadForm.reset();
+      closeUploadForm();
+    }
+
   }
 }
 
-const getUploadFormHandler = () => imageUploadInput.addEventListener('change', openUploadForm);
+const initUploadForm = () => imageUploadInput.addEventListener('change', openUploadForm);
 
 export {
-  getUploadFormHandler
+  initUploadForm
 };
 
 
